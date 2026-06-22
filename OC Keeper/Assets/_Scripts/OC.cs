ebd88ad3b26bbfe3501114bf92e29388489fc73c
Dooruss <doorusdebeijer@gmail.com>
2026-06-22 17:48:40 +0200

    FEAT: Oc appears yay

---
+++ OC Keeper/Assets/_Scripts/OC.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OC", menuName = "Scriptable Objects/OC")]
+public class OC : ScriptableObject
+{
+    // Main stuff
+    [Header("Main Info")]
+    public string Name;
+    public string Pronouns;
+    public int Age;
+    public string Birthday;
+    public float Height;
+    public enum Sexuality
+    {
+        straight , gay ,lesbian , bisexual , pansexual
+    }
+
+    // Relationships
+    // later im lazy asf
+
+    // Importaznt info
+    [Header("Important Info")]
+    public string Occupation;
+    public enum Ethnicity
+    {
+        America , german
+    }
+    public enum Species
+    {
+        Human , Fairy , Zombie , Skeleton , Demon , Angel , God , Werewolf , Vampire , Ghost , Witch , Mermaid , Siren
+    }
+    public enum Residance_Country
+    {
+        Opaline , Carolti , Berinvino , Zexom
+    }
+
+    // General info
+    [Header("General info")]
+    public string hobbys;
+    public bool Religious;
+    public string Signature_Weapon;
+    public string Weaknesses;
+    public string Fear_Phobia;
+    public string Fav_Food;
+    public string Fav_Color;
+    // Skills
+    [Header("Skills")]
+    public bool Able_To_Cook;
+    public bool Able_To_Bake;
+    public bool Able_To_Drive;
+    public string Able_To_Instruments;
+    //backstory
+    [Header("Backstory")]
+    public string Backstory;
+    //Appearance
+    [Header("Appearance")]
+    public string Body;
+    public string Eyes;
+    public string Hair;
+    public string Clothing;
+    public string Fantasy_Features;
+    public string Defining_Features;
+    public string Voice;
+}
---
+++ OC Keeper/Assets/_Scripts/OC_Info_Databse.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class OC_Info_Databse : MonoBehaviour
+{
+    public Transform contentParent;
+    public GameObject OCButtonPrefab;
+    public List<OC> AllOCs = new List<OC>();
+
+    public void OpenOCSDatabase()
+    {
+        // CLEAR OLD BUTTONS
+        foreach (Transform child in contentParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+
+        foreach (OC oc in AllOCs)
+        {
+            GameObject buttonObj = Instantiate(OCButtonPrefab, contentParent);
+            TMP_Text text = buttonObj.GetComponentInChildren<TMP_Text>();
+            Button button = buttonObj.GetComponent<Button>();
+            text.text = oc.name;
+
+            button.onClick.AddListener(() =>
+            {
+                // open oc ui
+                Debug.Log("Apple");
+            });
+
+        }
+    }
+
+}
